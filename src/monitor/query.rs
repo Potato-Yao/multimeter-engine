a95813aabfb26df7eded40de52b0a6fab69d5696
Potato-Yao <1120242312@bit.edu.cn>
2026-06-28 22:28:04 +0800

    make clippy happy

--- src/monitor/query.rs
+++ src/monitor/query.rs
@@ -1,10 +1,12 @@
 use crate::util::data_container::DataContainer;
 
+#[allow(unused)]
 pub trait QueryField {
     fn query(&self, key: &str) -> QueryResult;
 }
 
 /// the option inside Found means whether finding the corresponding field or not
+#[allow(unused)]
 #[derive(Debug)]
 pub enum QueryResult {
     Found(Option<DataContainer>),
